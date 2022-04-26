b462a36d43eb061ff90e1d3c8b6c463534ac9f22
Your Name <you@example.com>
2022-04-26 10:04:09 -0700

    responsive checking

--- src/app/views/company-admin/AddOutlet.jsx
+++ src/app/views/company-admin/AddOutlet.jsx
@@ -9,7 +9,7 @@ function AddOutlet() {
 
     
 
-<div className="container bg-white" >
+<div className="container bg-white table-wrapper" >
   <div className="row p-4 ">
   <div class="card" >
   <div class="card-body">
@@ -32,7 +32,7 @@ function AddOutlet() {
 
 
 
-  <div class="container-lg p-4" >
+  <div class="container-lg p-4 table-wrapper" >
 
 
     <div className="card p-3">
