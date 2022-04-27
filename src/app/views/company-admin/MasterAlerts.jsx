357893dfec4127d1383ba31ff4d536c8d6ed748a
Your Name <you@example.com>
2022-04-26 17:35:45 -0700

    making card anf bootstrao 5 icon link attexh

--- src/app/views/company-admin/MasterAlerts.jsx
+++ src/app/views/company-admin/MasterAlerts.jsx
@@ -4,16 +4,15 @@ function MasterAlerts() {
   return (<>
   
   <h1>MasterAlerts</h1>
-
+{/* 
 <div class="container">
 <div class=" row">
   <div className="col-lg-4 col-md-6 col-sm-10">
   <div class="card  ">
         <div class="backgroundEffect"></div>
         <div class="pic"> <img class="w-100" src="https://images.pexels.com/photos/374016/pexels-photo-374016.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt=""/>
-            {/* <div class="date"> <span class="day">26</span> <span class="month">June</span> <span class="year">2019</span> </div> */}
         </div>
-        <div class="content bg-warning">
+        <div class="content ">
             <p className='text-center fs-4'><strong>Cam-7</strong></p>
            <p className='ms-2'>Cam-location: <strong>Counter Front</strong></p>
            <p className='ms-2'>Outlet: <strong>Bilawal Chorangi</strong></p>
@@ -31,8 +30,37 @@ function MasterAlerts() {
 
    
 </div>
-</div>
+</div> */}
 
+
+
+
+<div class="container ">
+<div class=" row">
+  <div className="col-lg-4 col-md-6 col-sm-10 rounded">
+  <div class="card  ">
+        <div class="backgroundEffect"></div>
+        <div class="pic card-sub "> <img class="w-100" src="https://images.pexels.com/photos/374016/pexels-photo-374016.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt=""/>
+        </div>
+        <div class="content text-center">
+          <div className="py-2">
+
+          
+            <span className='text-center '><strong>Cam-7</strong></span>
+           <span className='ms-2 '><span className=''><i class="bi bi-camera-video-fill"></i></span> Counter Front</span>
+           <span className='ms-2'> <span className=''><i class="bi bi-geo-alt-fill"></i></span> Bilawal Chorangi</span>
+       </div>
+         
+            <div class="p-2 mt-1 border-top border-warning mx-5"> Status : <strong className='text-warning'>Not Working</strong> <span class="fas fa-arrow-right"></span></div>
+            <div class="p-2 border-top border-success mx-5">AI Status : <strong className='text-success'>No Alert</strong> <span class="fas fa-arrow-right"></span></div>
+        </div>
+    </div>
+
+  </div>
+
+   
+</div>
+</div>
 
 
 
